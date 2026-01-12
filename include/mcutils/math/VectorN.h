40148015862d57757d557bbdfb693f31e64c04ea
Marek Cel <marek.cel@gmail.com>
2026-01-12 17:35:17 +0100

    Refactor division operator for scalar to handle both unit and arithmetic types

--- include/mcutils/math/VectorN.h
+++ include/mcutils/math/VectorN.h
@@ -456,22 +456,6 @@ public:
         return result;
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     /**
      * \brief Multiplication by a scalar operator.
      * 
@@ -732,32 +716,6 @@ public:
 		}
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     /**
      * \brief Division by a scalar operator.
      * 
@@ -776,29 +734,51 @@ public:
         return result;
     }
 
+    /**
+     * \brief Division by a scalar operator.
+     * 
+     * This template is enabled when TYPE or TYPE_RHS is an arithmetic type
+     * while the other is a unit.
+     * 
+     * \tparam RHS_TYPE type of the other vector elements
+     * \param value value to be divided by
+     * \return vector divided by the value
+     */
     template <typename RHS_TYPE>
-    requires (std::is_arithmetic<TYPE>::value && units::traits::is_unit_t<RHS_TYPE>::value)
+    requires (
+        (std::is_arithmetic<TYPE>::value && units::traits::is_unit_t<RHS_TYPE>::value) ||
+        (units::traits::is_unit_t<TYPE>::value && std::is_arithmetic<RHS_TYPE>::value)
+    )
     auto operator/(const RHS_TYPE& value) const
     {
-        VectorN<
-            units::unit_t<
-                units::inverse<typename units::traits::unit_t_traits<RHS_TYPE>::unit_type>
-            >,
-            SIZE
-        > result;
-        multiplyVectorByValue(*this, 1.0 / value, &result);
-        return result;
+        if constexpr (units::traits::is_unit_t<TYPE>::value)
+        {
+            VectorN<TYPE, SIZE> result;
+            multiplyVectorByValue(*this, 1.0 / value, &result);
+            return result;
+        }
+        else
+        {
+            VectorN<
+                units::unit_t<
+                    units::inverse<typename units::traits::unit_t_traits<RHS_TYPE>::unit_type>
+                >,
+                SIZE
+            > result;
+            multiplyVectorByValue(*this, 1.0 / value, &result);
+            return result;
+        }
     }
 
-    template <typename RHS_TYPE>
-    requires (units::traits::is_unit_t<TYPE>::value && std::is_arithmetic<RHS_TYPE>::value)
-    auto operator/(const RHS_TYPE& value) const
-    {
-        VectorN<TYPE,SIZE> result;
-        multiplyVectorByValue(*this, 1.0 / value, &result);
-        return result;
-    }
-
+    /**
+     * \brief Division by a scalar operator.
+     * 
+     * This template is enabled when TYPE and RHS_TYPE are both units.
+     * 
+     * \tparam RHS_TYPE type of the other vector elements
+     * \param value value to be divided by
+     * \return vector divided by the value
+     */
     template <typename RHS_TYPE>
     requires (units::traits::is_unit_t<TYPE>::value && units::traits::is_unit_t<RHS_TYPE>::value)
     auto operator/(const RHS_TYPE& value) const
