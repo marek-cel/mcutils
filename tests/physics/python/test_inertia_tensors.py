fc6109e285d893f13fbc2cb6965aa295dfa29ad0
Marek Cel <marek.cel@gmail.com>
2026-01-25 12:16:51 +0100

    Add test for inertia tensors of various 3D shapes

---
+++ tests/physics/python/test_inertia_tensors.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+import trimesh
+
+mass = 1.0
+
+mesh_sphere = trimesh.load("sphere_r_1.stl")
+inertia_tensor_sphere = mesh_sphere.moment_inertia * mass
+print(inertia_tensor_sphere)
+
+
+mesh_cylinder = trimesh.load("cylinder_r_1_h_1.stl")
+inertia_tensor_cylinder = mesh_cylinder.moment_inertia * mass
+print(inertia_tensor_cylinder)
+
+mesh_cube = trimesh.load("cube_1x1x1.stl")
+inertia_tensor_cube = mesh_cube.moment_inertia * mass
+print(inertia_tensor_cube)
