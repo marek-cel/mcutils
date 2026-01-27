e965df21b44ead67fd40366dfcd94b0da519491d
Marek Cel <marek.cel@gmail.com>
2026-01-27 14:12:02 +0100

    Implement tests for inertia tensor calculations of solid spheres

--- tests/physics/TestInsertiaTensors.cpp
+++ tests/physics/TestInsertiaTensors.cpp
@@ -12,7 +12,29 @@ protected:
     void TearDown() override {}
 };
 
-TEST_F(TestInsertiaTensors, CanTODO)
+TEST_F(TestInsertiaTensors, CanCalculateInertiaTensorOfSolidSphere)
 {
-    // TODO: implement tests for inertia tensors
+    // expected values calculated with Python trimesh
+    // tests/physics/python/test_inertia_tensors.py
+    // [[ 1.63296006e+00 -1.46409247e-07 -1.49779016e-10]
+    // [-1.46409247e-07  1.63296001e+00 -2.26705792e-10]
+    // [-1.49779016e-10 -2.26705792e-10  1.62773793e+00]]
+
+    constexpr units::mass::kilogram_t mass = 1.0_kg;
+    constexpr units::length::meter_t radius = 1.0_m;
+
+    mc::Matrix3x3_kg_m_sq i = mc::physics::calculateInrtiaTensorOfSolidSphere(mass, radius);
+
+    constexpr double tolerance = 1.0e-6;
+
+    EXPECT_NEAR(i.xx().to<double>(), 1.63296006, tolerance);
+    EXPECT_NEAR(i.yy().to<double>(), 1.63296001, tolerance);
+    EXPECT_NEAR(i.zz().to<double>(), 1.62773793, tolerance);
+    EXPECT_NEAR(i.xy().to<double>(), -1.46409247e-07, tolerance);
+    EXPECT_NEAR(i.xz().to<double>(), -1.49779016e-10, tolerance);
+    EXPECT_NEAR(i.yz().to<double>(), -2.26705792e-10, tolerance);
+    EXPECT_NEAR(i.yx().to<double>(), -1.46409247e-07, tolerance);
+    EXPECT_NEAR(i.zx().to<double>(), -1.49779016e-10, tolerance);
+    EXPECT_NEAR(i.zy().to<double>(), -2.26705792e-10, tolerance);
+
 }
--- tests/physics/python/test_inertia_tensors.py
+++ tests/physics/python/test_inertia_tensors.py
@@ -6,14 +6,16 @@ import trimesh
 mass = 1.0
 
 mesh_sphere = trimesh.load("sphere_r_1.stl")
-inertia_tensor_sphere = mesh_sphere.moment_inertia * mass
+print(mesh_sphere.mass)
+inertia_tensor_sphere = mesh_sphere.moment_inertia / mesh_sphere.mass
 print(inertia_tensor_sphere)
 
 
-mesh_cylinder = trimesh.load("cylinder_r_1_h_1.stl")
-inertia_tensor_cylinder = mesh_cylinder.moment_inertia * mass
-print(inertia_tensor_cylinder)
+# mesh_cylinder = trimesh.load("cylinder_r_1_h_1.stl")
+# inertia_tensor_cylinder = mesh_cylinder.moment_inertia * mass
+# print(inertia_tensor_cylinder)
 
-mesh_cube = trimesh.load("cube_1x1x1.stl")
-inertia_tensor_cube = mesh_cube.moment_inertia * mass
-print(inertia_tensor_cube)
+
+# mesh_cube = trimesh.load("cube_1x1x1.stl")
+# inertia_tensor_cube = mesh_cube.moment_inertia * mass
+# print(inertia_tensor_cube)
