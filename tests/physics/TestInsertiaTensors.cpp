d6d7d928a1290bda34f7e3dd681fa684568aa719
Marek Cel <marek.cel@gmail.com>
2026-02-05 18:52:30 +0100

    Enable inertia tensors tests

--- tests/physics/TestInsertiaTensors.cpp
+++ tests/physics/TestInsertiaTensors.cpp
@@ -20,21 +20,21 @@ TEST_F(TestInsertiaTensors, CanCalculateInertiaTensorOfSolidSphere)
     // [-1.46409247e-07  1.63296001e+00 -2.26705792e-10]
     // [-1.49779016e-10 -2.26705792e-10  1.62773793e+00]]
 
-    // constexpr units::mass::kilogram_t mass = 1.0_kg;
-    // constexpr units::length::meter_t radius = 1.0_m;
+    constexpr units::mass::kilogram_t mass = 1.0_kg;
+    constexpr units::length::meter_t radius = 1.0_m;
 
-    // mc::Matrix3x3_kg_m_sq i = mc::physics::calculateInrtiaTensorOfSolidSphere(mass, radius);
+    mc::Matrix3x3_kg_m_sq i = mc::physics::calculateInrtiaTensorOfSolidSphere(mass, radius);
 
-    // constexpr double tolerance = 1.0e-6;
+    constexpr double tolerance = 1.0e-3;
 
-    // EXPECT_NEAR(i.xx().to<double>(), 1.63296006, tolerance);
-    // EXPECT_NEAR(i.yy().to<double>(), 1.63296001, tolerance);
-    // EXPECT_NEAR(i.zz().to<double>(), 1.62773793, tolerance);
-    // EXPECT_NEAR(i.xy().to<double>(), -1.46409247e-07, tolerance);
-    // EXPECT_NEAR(i.xz().to<double>(), -1.49779016e-10, tolerance);
-    // EXPECT_NEAR(i.yz().to<double>(), -2.26705792e-10, tolerance);
-    // EXPECT_NEAR(i.yx().to<double>(), -1.46409247e-07, tolerance);
-    // EXPECT_NEAR(i.zx().to<double>(), -1.49779016e-10, tolerance);
-    // EXPECT_NEAR(i.zy().to<double>(), -2.26705792e-10, tolerance);
+    EXPECT_NEAR(i.xx().to<double>(), 1.63296006, tolerance);
+    EXPECT_NEAR(i.yy().to<double>(), 1.63296001, tolerance);
+    EXPECT_NEAR(i.zz().to<double>(), 1.62773793, tolerance);
+    EXPECT_NEAR(i.xy().to<double>(), -1.46409247e-07, tolerance);
+    EXPECT_NEAR(i.xz().to<double>(), -1.49779016e-10, tolerance);
+    EXPECT_NEAR(i.yz().to<double>(), -2.26705792e-10, tolerance);
+    EXPECT_NEAR(i.yx().to<double>(), -1.46409247e-07, tolerance);
+    EXPECT_NEAR(i.zx().to<double>(), -1.49779016e-10, tolerance);
+    EXPECT_NEAR(i.zy().to<double>(), -2.26705792e-10, tolerance);
 
 }
---
+++ tests/physics/python/python_venv_create.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python3 -m venv .venv
+
+source ./python_venv_activate.sh
+
+pip install trimesh
